7cb63b1130d8d5c2ade6b788f02689f91ff2c044
ulnk <93608862+ulnk@users.noreply.github.com>
2022-12-11 14:00:26 +0000

    ++

--- src/buttons/verify.ts
+++ src/buttons/verify.ts
@@ -1,6 +1,7 @@
 import { ButtonInteraction } from "discord.js";
 import { prepareVerifyEmbed } from "../util/embeds/prepareVerify";
 import { prepareMessageVerifyEmbed } from "../util/embeds/prepareMessageVerify";
+import { failedToMessageEmbed } from "../util/embeds/failedToMessage";
 import { DiscordSocket } from "../structs/SocketClient";
 
 export default {
@@ -8,15 +9,25 @@ export default {
   async callback(interaction: ButtonInteraction) {
     const channel = await interaction.user.createDM();
     const messageEmbed = await prepareMessageVerifyEmbed();
-
+    const failedEmbed = await failedToMessageEmbed();
     const verifyEmbed = await prepareVerifyEmbed(channel.id);
+
+    const messagedUser = await channel
+      .send({ embeds: [messageEmbed] })
+      .catch(() => false);
+
+    // interaction.reply("hello");
+
+    if (!messagedUser)
+      return interaction.reply({
+        ephemeral: true,
+        embeds: [failedEmbed],
+      });
+
     interaction.reply({
       ephemeral: true,
       embeds: [verifyEmbed],
     });
-
-    channel.send({ embeds: [messageEmbed] });
-
     new DiscordSocket(interaction);
   },
 };
